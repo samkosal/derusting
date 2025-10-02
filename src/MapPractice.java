c65154c5e5592d36a3b0238ca80ed5081a015e3b
samkosal <67755908+samkosal@users.noreply.github.com>
2025-10-01 18:52:18 -0800

    MapPractice commit

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -1,29 +1,49 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapPractice {
   public static void main(String[] args) {
     // Create a HashMap with String keys and Integer values and
     // assign it to a variable of type Map
+    Map<String, Integer> Map = new HashMap<>();
 
     // Put 3 different key/value pairs in the Map
     // (it's OK to do this one-by-one)
+    Map.put("Apple", 1);
+    Map.put("Banana", 2);
+    Map.put("Orange", 3);
 
     // Get the value associated with a given key in the Map
+    Integer givenvalue = Map.get("Apple");
 
     // Find the size (number of key/value pairs) of the Map
+    Integer Size = Map.size();
 
     // Replace the value associated with a given key (the size of the Map shoukld not change)
-
+    Map.put("Apple", 4);
     // Check whether the Map contains a given key
+    if (Map.containsKey("Apple")) {
+      System.out.println(true);
+    }
 
     // Check whether the Map contains a given value
-
+    if (Map.containsValue(4)) {
+      System.out.println(true);
+    }
+    System.out.println();
     // Iterate over the keys of the Map, printing each key
-
+    for (String key : Map.keySet()) {
+      System.out.println(key);
+    }
     // Iterate over the values of the map, printing each value
+    for (Integer value : Map.values()) {
+      System.out.println(value);
+    }
 
     // Iterate over the entries in the map, printing each key and value
-
+    for (Map.Entry<String, Integer> entry : Map.entrySet()) {
+      System.err.println(entry);
+    }
     /*
      * Usage tip!
      * 
