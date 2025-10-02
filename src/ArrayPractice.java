84758dec69da8fd74ab0fbf5d9bd1c4ea83c7742
samkosal <67755908+samkosal@users.noreply.github.com>
2025-10-01 17:57:59 -0800

    ArrayPractice commit

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,17 +1,26 @@
 public class ArrayPractice {
   public static void main(String[] args) {
     // Create an array of Strings of size 4
+    String[] array = new String[4];
 
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
-
+    array[0] = "Apple";           
+    array[1] = "Cherry"; 
+    array[2] = "Orange";
+    array[3] = "Peach"; 
     // Get the value of the array at index 2
-
+    String Index2 = array[1];
     // Get the length of the array
-
+    Integer arrayLength = array.length;
     // Iterate over the array using a traditional for loop and print out each item
-
-    // Iterate over the array using a for-each loop and print out each item
+    for (int i = 0; i < arrayLength; i++) {
+      System.out.println(array[i]);
+    }
+    // Iterate over the array using a for-each loop and print out each item\
+    for (String word : array) {
+      System.out.println(word);
+    }
 
     /*
      * Reminder!
--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,7 +1,7 @@
 public class StringPractice {
   public static void main(String[] args) {
     // Create a string with at least 5 characters and assign it to a variable
-
+// df
     // Find the length of the string
 
     // Concatenate (add) two strings together and reassign the result
