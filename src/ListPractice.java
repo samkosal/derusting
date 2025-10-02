81c6541de49881a3da99d990e12382fe711460a6
samkosal <67755908+samkosal@users.noreply.github.com>
2025-10-01 18:32:48 -0800

    ListPractice commit

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,27 +1,49 @@
-public class ListPractice {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
+public class ListPractice {
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
+    List<String> ArrayList = new ArrayList<>();
 
     // Add 3 elements to the list (OK to do one-by-one)
-
+    ArrayList.add("Apple");
+    ArrayList.add("Cherry");
+    ArrayList.add("Orange");        
     // Print the element at index 1
+    System.out.println(ArrayList.get(1));
 
     // Replace the element at index 1 with a new value
+    ArrayList.set(1, "Peach");
+    
     // (Do not insert a new value. The length of the list should not change)
 
     // Insert a new element at index 0 (the length of the list will change)
+    ArrayList.add(0, "Banana");
 
     // Check whether the list contains a certain string
-
+    // which String?
+    if (ArrayList.contains("Peach")) {
+      System.out.println("Peach");
+    } 
     // Iterate over the list using a traditional for-loop.
-    // Print each index and value on a separate line
+    for (int i = 0; i < ArrayList.size(); i++) {
+      // Print each index and value on a separate line
+      System.out.println("Index:" + i);
+      System.out.println("Value:" + ArrayList.get(i));
+      System.out.println("");
+    }
 
     // Sort the list using the Collections library
+    Collections.sort(ArrayList);
 
     // Iterate over the list using a for-each loop
     // Print each value on a second line
+    for (String word : ArrayList) {
+      System.out.println(word);
+    }
 
     /*
      * Usage tip!
